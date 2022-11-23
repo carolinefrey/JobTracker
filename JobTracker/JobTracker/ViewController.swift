de5271d05c22abc1e821fdadbe58876e61543c0c
Caroline Frey <cfrey9@gmail.com>
2022-11-23 15:34:43 -0500

    Initial commit

---
+++ JobTracker/JobTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JobTracker
+//
+//  Created by Caroline Frey on 11/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
